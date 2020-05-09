d22eaeba937b64f487a469c8b6cb23acdd9b07a3
Rinat Sagitov <sagitovrf@gmail.com>
2020-05-09 16:12:02 +0500

    Fix

--- kotlin.5.3.Generics/src/evenList.kt
+++ kotlin.5.3.Generics/src/evenList.kt
@@ -1,3 +1,26 @@
 fun evenList(list: List<Number>): List<Number> {
-    return list.filter { it.toByte() % 2 == 0 }
+    return list.filter { it.toLong() % 2 == 0L }
+}
+
+fun <T : Number> evenListWithoutMaxLong(list: List<T>): List<T> {
+    return list.filter { kotlin.math.round(it.toDouble()) == it.toDouble() }.filter { it.toLong() % 2 == 0L }
+}
+
+fun <T : Number> evenListWithMaxLong(list: List<T>): List<T> {
+    val evenList: MutableList<T> = mutableListOf()
+    for (x in list) {
+        if (x is Long && (x == Long.MAX_VALUE)) {
+            continue
+        }
+
+        val l = x.toLong()
+        if (l < Long.MAX_VALUE) {
+            if (kotlin.math.round(x.toDouble()) == x.toDouble() && l % 2 == 0L) {
+                evenList.add(x)
+            }
+        } else {
+            evenList.add(x)
+        }
+    }
+    return evenList
 }
