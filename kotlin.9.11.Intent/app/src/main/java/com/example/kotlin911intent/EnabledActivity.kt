a32d5519b0c0c23b5e722b4a80102b09cba6d44b
Rinat Sagitov <sagitovrf@gmail.com>
2020-05-30 09:31:02 +0500

    Add 9.11 Intent ActivityResultContracts Homework

--- kotlin.9.11.Intent/app/src/main/java/com/example/kotlin911intent/EnabledActivity.kt
+++ kotlin.9.11.Intent/app/src/main/java/com/example/kotlin911intent/EnabledActivity.kt
@@ -1,11 +1,68 @@
 package com.example.kotlin911intent
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_enabled.*
 
 class EnabledActivity : AppCompatActivity() {
 
+    companion object {
+        private const val DIAL_REQUEST_CODE = 100
+    }
+
+    //Do not work with alpha05, do not have dial method
+    private val dialLauncher = prepareCall(ActivityResultContracts.Dial()) { result ->
+        //    Did not answer with "We called"
+        if (result) {
+            println(result.toString())
+            toast("We did call")
+        } else {
+            println(result.toString())
+            toast("We did not call")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enabled)
+
+        callButton.setOnClickListener {
+            val phoneNumber = phoneNumberEditText.text.toString()
+            val isPhoneNumberValid = Patterns.PHONE.matcher(phoneNumber).matches()
+            if (!isPhoneNumberValid) {
+                toast("Type valid phone number")
+            } else {
+                dialLauncher.launch(phoneNumber)
+//                val phoneIntent = Intent(Intent.ACTION_DIAL).apply {
+//                    data = Uri.parse("tel:$phoneNumber")
+//                }
+//                if (phoneIntent.resolveActivity(packageManager) != null) {
+//                    startActivityForResult(phoneIntent, DIAL_REQUEST_CODE)
+//                }
+            }
+        }
+    }
+//    Did not answer with "We called"
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        if (requestCode == DIAL_REQUEST_CODE) {
+//            if (resultCode == Activity.RESULT_OK) {
+//                toast("We did call")
+//            } else {
+//                toast("We did not call")
+//            }
+//        } else {
+//            super.onActivityResult(requestCode, resultCode, data)
+//        }
+//    }
+
+    private fun toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- kotlin.9.11.Intent/app/src/main/java/com/example/kotlin911intent/MainActivity.kt
+++ kotlin.9.11.Intent/app/src/main/java/com/example/kotlin911intent/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.kotlin911intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
-import android.view.View
+import android.util.Patterns
 import android.widget.*
-import androidx.constraintlayout.widget.ConstraintSet
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         private const val FORM_STATE = "Form state"
     }
 
-    private val tag = "MainActivity"
     private var state: FormState? = FormState(true, "")
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -38,48 +34,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.v(tag, "onCreate was called")
-        Log.d(tag, "onCreate was called")
-        Log.i(tag, "onCreate was called")
-        Log.w(tag, "onCreate was called")
-        Log.e(tag, "onCreate was called")
-
-//        val progressBar = ProgressBar(this).apply {
-//            id = View.generateViewId()
-//            layoutParams = ConstraintLayout.LayoutParams(
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-//                ConstraintLayout.LayoutParams.MATCH_PARENT
-//            ).apply {
-//                endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
-//                startToStart = ConstraintLayout.LayoutParams.PARENT_ID
-//            }
-//        }
-//        container.addView(progressBar)
-
-//        val progressBar = ProgressBar(this).apply {
-//            id = View.generateViewId()
-//        }
-//        container.addView(progressBar)
-//        ConstraintSet().apply {
-//            constrainHeight(progressBar.id, ConstraintSet.WRAP_CONTENT)
-//            constrainWidth(progressBar.id, ConstraintSet.WRAP_CONTENT)
-//            connect(progressBar.id,ConstraintSet.LEFT,ConstraintSet.PARENT_ID,ConstraintSet.LEFT)
-//            connect(progressBar.id,ConstraintSet.RIGHT,ConstraintSet.PARENT_ID,ConstraintSet.RIGHT)
-//            connect(progressBar.id,ConstraintSet.TOP,ConstraintSet.PARENT_ID,ConstraintSet.TOP)
-//            connect(progressBar.id,ConstraintSet.BOTTOM,ConstraintSet.PARENT_ID,ConstraintSet.BOTTOM)
-//            setMargin(progressBar.id, ConstraintSet.LEFT, 110)
-//            applyTo(container)
-//        }
-
         val imageLink =
             "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
         Glide.with(this).load(imageLink).into(imageView)
 
         emailEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                if (!emailEditText.isEmailValid()) emailEditText.error =
-                    "Please Enter Email Address"
-            }
+            override fun afterTextChanged(s: Editable?) {}
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -89,9 +49,7 @@ class MainActivity : AppCompatActivity() {
         })
 
         passEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                if (passEditText.text.isBlank()) passEditText.error = "Please Enter Password"
-            }
+            override fun afterTextChanged(s: Editable?) {}
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -105,120 +63,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         loginButton.setOnClickListener {
-            if (emailEditText.text.toString().substringAfter('@') != "gmail.com") {
-                emailEditText.error = "Email must be gmail.com"
-                stateTextView.text = getString(R.string.form_not_true)
-                state = FormState(false, getString(R.string.form_not_true))
-            }
-            if (passEditText.text.length < 10) {
-                passEditText.error = "Password must be 10 characters"
-                stateTextView.text = getString(R.string.form_not_true)
-                state = FormState(false, getString(R.string.form_not_true))
-            } else {
-                stateTextView.text = ""
-                state = FormState(true, "")
-                makeLongBar()
-            }
+            val enabledActivityIntent = Intent(this, EnabledActivity::class.java)
+            startActivity(enabledActivityIntent)
         }
 
-        imageView.setOnClickListener {
-            Thread.sleep(6000)
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.v(tag, "onStart was called")
-        Log.d(tag, "onStart was called")
-        Log.i(tag, "onStart was called")
-        Log.w(tag, "onStart was called")
-        Log.e(tag, "onStart was called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.v(tag, "onResume was called")
-        Log.d(tag, "onResume was called")
-        Log.i(tag, "onResume was called")
-        Log.w(tag, "onResume was called")
-        Log.e(tag, "onResume was called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.v(tag, "onPause was called")
-        Log.d(tag, "onPause was called")
-        Log.i(tag, "onPause was called")
-        Log.w(tag, "onPause was called")
-        Log.e(tag, "onPause was called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.v(tag, "onStop was called")
-        Log.d(tag, "onStop was called")
-        Log.i(tag, "onStop was called")
-        Log.w(tag, "onStop was called")
-        Log.e(tag, "onStop was called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.v(tag, "onDestroy was called")
-        Log.d(tag, "onDestroy was called")
-        Log.i(tag, "onDestroy was called")
-        Log.w(tag, "onDestroy was called")
-        Log.e(tag, "onDestroy was called")
     }
 
     private fun EditText.isEmailValid(): Boolean {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(this.text.toString()).matches()
+        return Patterns.EMAIL_ADDRESS.matcher(this.text.toString()).matches()
     }
 
     private fun isAuthValid(): Boolean {
-        return emailEditText.isEmailValid() && passEditText.text.isNotBlank() && checkBox.isChecked
-    }
-
-    private fun makeLongBar() {
-        emailEditText.isEnabled = false
-        passEditText.isEnabled = false
-        checkBox.isEnabled = false
-        loginButton.isEnabled = false
-
-        val progressBar = ProgressBar(this).apply {
-            id = View.generateViewId()
-        }
-        container.addView(progressBar)
-        val set = ConstraintSet().apply {
-            constrainHeight(progressBar.id, ConstraintSet.WRAP_CONTENT)
-            constrainWidth(progressBar.id, ConstraintSet.WRAP_CONTENT)
-            connect(progressBar.id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT)
-            connect(
-                progressBar.id,
-                ConstraintSet.RIGHT,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.RIGHT
-            )
-            connect(progressBar.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
-            connect(
-                progressBar.id,
-                ConstraintSet.BOTTOM,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.BOTTOM
-            )
-            applyTo(container)
-        }
-
-        Handler().postDelayed({
-            emailEditText.text.clear()
-            emailEditText.isEnabled = true
-            passEditText.text.clear()
-            passEditText.isEnabled = true
-            checkBox.isEnabled = true
-            checkBox.isChecked = false
-            loginButton.isEnabled = true
-            container.removeView(progressBar)
-            Toast.makeText(this, "No connection", Toast.LENGTH_SHORT).show()
-        }, 2000)
+        return emailEditText.isEmailValid()
+                && emailEditText.text.toString().substringAfter('@') == "gmail.com"
+                && passEditText.text.isNotBlank()
+                && checkBox.isChecked
     }
 }
