5469a489ad4547df09f79385df963bbcf0a142a7
Rinat Sagitov <sagitovrf@gmail.com>
2020-06-01 12:53:15 +0500

    Fix

--- kotlin.9.11.Intent/app/src/main/java/com/example/kotlin911intent/EnabledActivity.kt
+++ kotlin.9.11.Intent/app/src/main/java/com/example/kotlin911intent/EnabledActivity.kt
@@ -1,12 +1,8 @@
 package com.example.kotlin911intent
 
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_enabled.*
